ffa4747425f501e62f66f12f141129b3127515e6
flamingfox <julien.brillet@yahoo.fr>
2016-01-13 16:36:55 +0100

    modification voxel

--- modele/voxel.cpp
+++ modele/voxel.cpp
@@ -1,9 +1,88 @@
 #include "voxel.h"
 
-Voxel::Voxel():_n(0){
+Voxel::Voxel():_nbCubeParDimension(0), _posMin(0,0,0), _posMax(1,1,1){
 }
 
-Voxel::Voxel(int n):_n(n){
-    v.resize(n*n*n);
+Voxel::Voxel(const Vector3D &posMin, const Vector3D &posMax, const int &nbCubeParDimension):_nbCubeParDimension(nbCubeParDimension), _posMin(posMin), _posMax(posMax){
+    v.resize(nbCubeParDimension*nbCubeParDimension*nbCubeParDimension);
 }
 
+/*
+Voxel Voxel::operator +(const Voxel &v)
+{
+    Voxel retour;
+
+    Vector3D posMin, posMax;
+
+    if(v._posMin < this->_posMin)
+        posMin = v._posMin;
+    else
+        posMin = this->_posMin;
+
+    if(v._posMax > this->_posMax)
+        posMax = v._posMax;
+    else
+        posMax = this->_posMax;
+
+
+    float distanceDimensionX = posMax.x - posMin.x;
+    Vector3D dimensionCubeUnitaire;
+
+    if(v.getDimensionCubeUnitaire() < this->getDimensionCubeUnitaire())
+        dimensionCubeUnitaire = v.getDimensionCubeUnitaire();
+    else
+        dimensionCubeUnitaire = this->getDimensionCubeUnitaire();
+
+    int nbCubeParDimension = (int) (roundf(distanceDimensionX/dimensionCubeUnitaire.x));
+
+    posMax = posMin + dimensionCubeUnitaire*nbCubeParDimension;
+
+    retour = Voxel(posMin,posMax, nbCubeParDimension);
+
+    Vector3D dimCubeUnitaire = retour.getDimensionCubeUnitaire();
+
+    for(int i=0; i<nbCubeParDimension; i++){
+        for(int j=0; j<nbCubeParDimension; j++){
+            for(int k=0; k<nbCubeParDimension; k++){
+
+                Vector3D tmp = retour.getPosMin();
+                tmp.x += (i+0.5)*dimCubeUnitaire.x;
+                tmp.y += (j+0.5)*dimCubeUnitaire.y;
+                tmp.z += (k+0.5)*dimCubeUnitaire.z;
+
+                if(v.getIndiceCase(tmp) != -1 && v.v[v.getIndiceCase(tmp)] != 0){
+                    retour(i,j,k) = 1;
+                }
+
+                if(this->getIndiceCase(tmp) != -1 && this->v[this->getIndiceCase(tmp)] != 0){
+                    retour(i,j,k) = 1;
+                }
+            }
+        }
+    }
+
+    return retour;
+
+
+}
+*/
+
+int Voxel::getIndiceCase(const Vector3D &position) const
+{
+    //Test hors zone voxel
+    if(position < this->_posMin || position > this->_posMax){
+        return -1;
+    }
+
+    float dimensionCubeUnitaire = (_posMax.x-_posMin.x)/_nbCubeParDimension;
+
+    Vector3D positionRelative = position - _posMin;
+
+    Vector3D indices = positionRelative / dimensionCubeUnitaire;
+
+    int indiceZ = (int)(indices.z)*_nbCubeParDimension*_nbCubeParDimension;
+    int indiceY = (int)(indices.y)*_nbCubeParDimension;
+    int indiceX = (int)(indices.x);
+
+    return (int)(indices.z)*_nbCubeParDimension*_nbCubeParDimension+(int)(indices.y)*_nbCubeParDimension+(int)(indices.x);
+}
--- modele/voxel.h
+++ modele/voxel.h
@@ -2,20 +2,32 @@
 #define VOXEL_H
 #include "vector"
 #include "string"
+#include "geometrie/vector3d.h"
 
 class Voxel
 {
 public:
     Voxel();
-    Voxel(int n);
+    Voxel(const Vector3D& posMin, const Vector3D& posMax, const int& nbCubeParDimension);
 
-    int operator()(int i, int j, int k)const{return v.at(i*_n*_n+j*_n+k);}
-    int& operator()(int i, int j, int k){return v[i*_n*_n+j*_n+k];}
-    inline int get_n()const{return _n;}
+    int operator()(int i, int j, int k)const{return v.at(i*_nbCubeParDimension*_nbCubeParDimension+j*_nbCubeParDimension+k);}
+    int& operator()(int i, int j, int k){return v[i*_nbCubeParDimension*_nbCubeParDimension+j*_nbCubeParDimension+k];}
+    inline int getNbCubeParDimension()const{return _nbCubeParDimension;}
+    inline Vector3D getPosMin()const{return _posMin;}
+    inline void setPosMin(const Vector3D& posMin){_posMin=posMin;}
+    inline Vector3D getPosMax()const{return _posMax;}
+    inline void setPosMax(const Vector3D& posMax){_posMax=posMax;}
+
+    inline Vector3D getDimensionCubeUnitaire() const{return (_posMax - _posMin)/_nbCubeParDimension ;}
+
+    //Voxel operator +(const Voxel& v);
 
 private:
+    Vector3D _posMin, _posMax;
     std::vector<int>v;
-    int _n;
+    int _nbCubeParDimension;
+
+    int getIndiceCase(const Vector3D& position) const;
 };
 
 #endif // VOXEL_H
